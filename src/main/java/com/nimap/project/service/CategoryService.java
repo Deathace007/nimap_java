e31004b87b0065da1eec6fd8e7581734c6a9f1a8
mohan <mohanborle17@gmail.com>
2025-04-05 09:10:10 +0530

    changes

--- src/main/java/com/nimap/project/service/CategoryService.java
+++ src/main/java/com/nimap/project/service/CategoryService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
@@ -15,23 +16,28 @@ public class CategoryService {
     @Autowired
     private CategoryRepository categoryRepository;
 
+    @Transactional
     public Page<Category> getAllCategories(Pageable pageable) {
         return categoryRepository.findAll(pageable);
     }
 
+    @Transactional
     public Category createCategory(Category category) {
         return categoryRepository.save(category);
     }
 
+    @Transactional
     public Optional<Category> getCategoryById(Long id) {
         return categoryRepository.findById(id);
     }
 
+    @Transactional
     public Category updateCategory(Long id, Category category) {
         category.setId(id);
         return categoryRepository.save(category);
     }
 
+    @Transactional
     public void deleteCategory(Long id) {
         categoryRepository.deleteById(id);
     }
